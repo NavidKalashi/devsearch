7ccc161dd64f6a215b87bc2fba870d7675884e26
NavidKalashi <kalashinavid@gmail.com>
2023-09-12 18:00:20 +0330

    user registration

--- users/views.py
+++ users/views.py
@@ -4,8 +4,10 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 
 from .models import Profile
+from .forms import CustomUserCreationForm
 
 def loginUser(request):
+    page = 'login'
     
     if request.user.is_authenticated:
         return redirect('profiles')
@@ -28,6 +30,24 @@ def loginUser(request):
             messages.error(request, 'Username or passwrod is incorrect')
         
     return render(request, 'users/login_register.html')
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'User account was created!')
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, 'An error has occurred during registeration')
+    
+    context = {'page': page, 'form': form}
+    return render(request, 'users/login_register.html', context)
 
 def logoutUser(request):
     logout(request)
